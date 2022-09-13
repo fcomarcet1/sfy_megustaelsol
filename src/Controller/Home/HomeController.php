96c4bf1c2d76a220dfe1f682750d3875d57bdc23
fcomarcet1 <fcomarcet1@gmail.com>
2022-09-13 17:11:10 +0200

    Add NewsController and route

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Home;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
